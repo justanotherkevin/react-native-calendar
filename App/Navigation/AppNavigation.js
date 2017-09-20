9a392b90129974ed26ba72e0b59f1eb3df4135a1
kevin hu <hu.kevin.w@gmail.com>
2017-09-20 12:03:40 -0400

    First commit

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,12 @@
 import { StackNavigator } from 'react-navigation'
+import CalendarScreen from '../Containers/CalendarScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  CalendarScreen: { screen: CalendarScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
